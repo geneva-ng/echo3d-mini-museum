9aec64536aed37940edb8e4ad3ef462d262baf4c
geneva-ng <123778388+geneva-ng@users.noreply.github.com>
2024-03-12 01:02:31 -0400

    Update bernini.js with new relative positions

--- js/bernini.js
+++ js/bernini.js
@@ -1,80 +1,79 @@
 export const bernini = [
     {
-        "position": "-9 1.5 7.2",
+        "position": "-7.2 1.5 -9.0",
         "scale": "1.5 8 1.5",
         "headerText": "Apollo and Daphne",
         "date": "1622",
         "bodyText": "Bernini's sculpture of Apollo and Daphne captures the dramatic transformation of Daphne into a tree as Apollo reaches for her, designed to be viewed from the right to fully appreciate the narrative and emotional intensity. Although initially intended to be seen from a specific angle, revealing intricate details and supporting structures, it was ultimately displayed centrally for all-around viewing. Like Bernini's earlier work, it features a moral aphorism by Pope Urban VIII, merging Christian values with a pagan story to justify its placement in the Borghese villa."
     },
     {
-        "position": "-2.5 1.5 -0.2",
+        "position": "0.2 1.5 -2.5",
         "scale": "2.3 7 3.5",
-        "headerText": "David (bernini)",
+        "headerText": "David",
         "date": "1598",
         "bodyText": "This is the only sculpture with a Biblical subject executed by Gian Lorenzo Bernini for Scipione Borghese. It depicts David in the instant before he hurls the rock that will strike the giant Goliath, whom the Philistines called upon to fight the Israelite army of King Saul. The cuirass lent to him by Saul lies on the ground together with a cithara, the hero’s traditional attribute. Here the instrument terminates in an eagle’s head, a clear indication of the intention to celebrate the house of the Borghese."
     },
     {
-        "position": "0 1.5 -12",
+        "position": "12.0 1.5 -0.0",
         "scale": "6 13 2",
         "headerText": "Habakkuk and the Angel",
         "date": "1655",
         "bodyText": "Bernini's \"Habakkuk and the Angel\" is a dynamic marble masterpiece from the Italian Baroque, depicting the prophet Habakkuk, guided by an angel to aid Daniel. Crafted for the Chigi Chapel, this sculpture showcases Bernini's ability to convey motion and emotion, with the angel's authoritative gesture and the detailed expressions of both figures highlighting divine intervention."
     },
     {
-        "position": "5 1.5 4.5",
+        "position": "-4.5 1.5 5.0",
         "scale": "2 8 2",
         "headerText": "Portrait of a Cardinal",
         "date": "1630",
         "bodyText": "\"Portrait of a Cardinal\" is one of Gian Lorenzo Bernini's most celebrated sculptural portraits, believed to have been created in the 1630s. This work exemplifies Bernini's mastery in capturing not just the physical likeness but also the character and dignity of his subjects. Unlike his dynamic narrative sculptures, here Bernini focuses on the subtlety of expression and the intricate details of the cardinal's vestments, conveying a sense of contemplative stillness and authority."
     },
     {
-        "position": "-9.5 1.5 -7.5",
+        "position": "7.5 1.5 -9.5",
         "scale": "3 8 3",
         "headerText": "Neptune and Triton",
         "date": "1622",
         "bodyText": "Neptune and Triton was commissioned from Bernini by Alessandro Peretti, an Italian Roman Catholic Cardinal Bishop, for the garden of the Villa Montalto in Rome. It was originally the centerpiece of a complex system of fountains and cascades located at the upper end of a large oval fishpond. In the 17th and 18th centuries this impressive sculptural group was one of the most celebrated sights in Rome."
     },
     {
-        "position": "10.8 1.5 -10.5",
+        "position": "10.5 1.5 10.8",
         "scale": "4 4 3",
         "headerText": "Resting Goat",
         "date": "1605",
         "bodyText": "Its legs outstretched, neck and head slightly inclined to the left and the soft moulding of the locks are the result of an admirable seventeenth-century intervention, which modern restorations have made it possible to trace back to the prodigious hand of Gian Lorenzo Bernini. Perhaps conceived in ancient times as part of the furnishings of a Roman residence, the sculpture of the mighty animal, dating back to the Imperial era, was acquired by Marquis Vincenzo Giustiniani, who decided to place it in the famous Gallery of Palazzo Giustiniani."
     },
     {
-        "position": "10.2 1.5 12.3",
+        "position": "-12.3 1.5 10.2",
         "scale": "1.4 8 1.4",
         "headerText": "Louis XIV",
         "date": "1665",
         "bodyText": "Bernini's bust of Louis XIV combines idealized grandeur with lifelike detail, portraying the French king as a heroic figure reminiscent of Alexander the Great. The sculpture, intended to emphasize Louis's majesty through symbolic armor and a commanding pose, skillfully blends nobility with precise personal features. Though planned to stand on a globe pedestal symbolizing global dominion, this element was never realized, and the bust now resides in the Palace of Versailles, showcasing Bernini's ability to fuse abstract ideals with realistic portrayal."
     },
     {
-        "position": "4.5 1.5 -4.2",
+        "position": "4.2 1.5 4.5",
         "scale": "2 8 2",
         "headerText": "Salvator Mundi",
         "date": "1679",
         "bodyText": "The Bust of the Saviour (Salvator Mundi) is the last sculpture created by Bernini, who died from the after-effects of a stroke, when the artist was 81 years old. He left the sculpture in his will to his friend and patron queen Christina of Sweden. Considered lost and \"rediscovered\" in 2001, by Francesco Petrucci, it is currently held in the basilica of San Sebastiano fuori le mura in Rome."
     },
     {
-        "position": "-0.5 1.5 12",
+        "position": "-12.0 1.5 -0.5",
         "scale": "5 11 2",
+        "headerText": "Abundance (Part of the Trevi Fountain Ensemble)",
+        "date": "1762",
+        "bodyText": "The sculpture of Abundance is an integral element of the majestic Trevi Fountain, the largest Baroque fountain in Rome and one of the most famous fountains in the world. This piece represents abundance, a theme central to the fountain's overall depiction of water as a source of life, prosperity, and renewal. The figure, elegantly carved in marble, is seen holding a horn of plenty, overflowing with fruits and grains, symbolizing the bountiful gifts of nature."
+    },
+    {
+        "position": "-11.6 1.5 8.0",
+        "scale": "1.2 6 1.2",
         "headerText": "Bust of Marsyas",
         "date": "1651",
         "bodyText": "In one of the great parables of hubris, the satyr Marsyas challenged the god Apollo to a musical competition. The god triumphed and then punished his challenger by skinning him alive. Here, Permoser condensed the tragic climax of the story into an emotive portrayal of Marsyas. The contorted face, with an open mouth revealing a bitten tongue, and twisting head immediately convey pain, while the taut shoulders suggest that his arms are bound behind his back. The satyr is draped with an animal pelt, pointing to his gruesome fate. The panels of mottled marble set into the base are flecked with red, perhaps evoking spattered blood."
     },
     {
-        "position": "8 1.5 11.6",
+        "position": "-11.4 1.5 12.3",
         "scale": "1.2 6 1.2",
         "headerText": "Bust of Medusa",
         "date": "1640",
         "bodyText": "Believed to date from between1638 and 1648, this extraordinary work takes its subject from classical mythology, as cited in Ovid’s Metamorphoses. It shows the beautiful Medusa, one of the Gorgon sisters, caught in the terrible process of transformation into a monster. Her hair is turning into writhing snakes, which, according to Ovid, was a punishment from Minerva for having had an affair with Neptune, god of the sea. The punishment also made Medusa an instrument of death by turning anyone who looked upon her to stone. Famously, Perseus overcame Medusa’s curse by looking at her reflection in a shield to behead her."
-    },
-    {
-        "position": "12.3 1.5 11.4",
-        "scale": "1.2 6 1.2",
-        "headerText": "Abundance (Part of the Trevi Fountain Ensemble)",
-        "date": "1762",
-        "bodyText": "The sculpture of Abundance is an integral element of the majestic Trevi Fountain, the largest Baroque fountain in Rome and one of the most famous fountains in the world. This piece represents abundance, a theme central to the fountain's overall depiction of water as a source of life, prosperity, and renewal. The figure, elegantly carved in marble, is seen holding a horn of plenty, overflowing with fruits and grains, symbolizing the bountiful gifts of nature."
-    }
-    ]
-
+   }
+]
